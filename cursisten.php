f3261f07d3b06ccc2401935e7eac931a5444e3a5
iMath97 <54057144+iMath97@users.noreply.github.com>
2019-10-30 11:21:33 +0100

    added titles

--- cursisten.php
+++ cursisten.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="description" content="Op deze pagina kun je alle cursisten vinden met een link naar bijhorende Linkedin pagina.">
 
-    <title>jobdag webcreators aalst</title>
+    <title>cursisten | jobdag webcreators aalst</title>
     <link rel="apple-touch-icon" sizes="180x180" href="./img/favicon/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="./img/favicon/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="./img/favicon/favicon-16x16.png">
